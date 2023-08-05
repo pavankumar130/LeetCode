01751be84eed915e64dc9433fa08a5440d473371
meravath pavan kumar <122618703+pavankumar130@users.noreply.github.com>
2023-08-05 15:06:52 +0530

    Time: 28 ms (96.44%) | Memory: 19 MB (43.81%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -5,20 +5,26 @@ public:
         sort(intervals.begin(),intervals.end());
         vector<vector<int>>ans;
         for(int i=0;i<n;i++){
-            int s=intervals[i][0];
-            int e=intervals[i][1];
-            if(!ans.empty() && e<=ans.back()[1]){
-                continue;
+            // int s=intervals[i][0];
+            // int e=intervals[i][1];
+            // if(!ans.empty() && e<=ans.back()[1]){
+            //     continue;
+            // }
+            // for(int j=i+1;j<n;j++){
+            //     if(intervals[j][0] <= e){
+            //         e=max(e,intervals[j][1]);
+            //     }
+            //     else{
+            //         break;
+            //     }
+            // }
+            // ans.push_back({s,e});
+            if(ans.empty() || intervals[i][0] >ans.back()[1]){
+                ans.push_back(intervals[i]);
             }
-            for(int j=i+1;j<n;j++){
-                if(intervals[j][0] <= e){
-                    e=max(e,intervals[j][1]);
-                }
-                else{
-                    break;
-                }
+            else{
+                ans.back()[1]=max(ans.back()[1],intervals[i][1]);
             }
-            ans.push_back({s,e});
         }
         return ans;
     }
