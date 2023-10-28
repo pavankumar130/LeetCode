9a81de793dcdda142cd1322b317e432fcdca9899
meravath pavan kumar <122618703+pavankumar130@users.noreply.github.com>
2023-10-28 17:06:34 +0530

    Time: 466 ms (6.50%) | Memory: 136.4 MB (11.78%) - LeetSync

--- 77-combinations/combinations.cpp
+++ 77-combinations/combinations.cpp
@@ -1,29 +1,25 @@
 class Solution {
 public:
-void solve(vector<int>temp,vector<vector<int>>&res, vector<int>&dp, vector<int>&arr,int n, int k, int ind){
+void solve(vector<int>temp,vector<vector<int>>&res, vector<int>&dp,int n, int k, int ind){
     if(temp.size() == k){
         res.push_back(temp);
         return;
     }
-    for(int i=ind;i<n;i++){
-        if(!dp[i]){
-            temp.push_back(arr[i]);
-            dp[i]=1;
-            solve(temp,res,dp,arr,n,k,i);
+    for(int i=ind;i<=n;i++){
+        if(!dp[i-1]){
+            temp.push_back(i);
+            dp[i-1]=1;
+            solve(temp,res,dp,n,k,i);
             temp.pop_back();
-            dp[i]=0;
+            dp[i-1]=0;
         }
     }
 }
     vector<vector<int>> combine(int n, int k) {
-        vector<int>arr(n);
-        for(int i=1;i<=n;i++){
-           arr[i-1]=i;
-        }
         vector<int>dp(n,0);
         vector<vector<int>>res;
         vector<int>temp;
-        solve(temp,res,dp,arr,n,k,0);
+        solve(temp,res,dp,n,k,1);
         sort(res.begin(),res.end());
         return res;
     }
