d31e3a4fe80e3a2ab9fee4b12f59afd52d520bf7
meravath pavan kumar <122618703+pavankumar130@users.noreply.github.com>
2023-10-28 17:12:09 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (14.74%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
-void solve(vector<int>&temp, vector<int>&nums, vector<int>&vis, vector<vector<int>>&ans){
-  if(temp.size() == nums.size()){
-      ans.push_back(temp);
-      return;
-  }
-  for(int i=0;i<nums.size();i++){
-      if(!vis[i]){
-          vis[i]=1;
-          temp.push_back(nums[i]);
-          solve(temp,nums,vis,ans);
-          vis[i]=0;
-          temp.pop_back();
-      }
-  }
+void solve(vector<int>temp,vector<vector<int>>&res,vector<int>&dp,vector<int>& nums){
+    if(temp.size() == nums.size()){
+        res.push_back(temp);
+        return;
+    }
+    for(int i=0;i<nums.size();i++){
+        if(!dp[i]){
+            dp[i]=1;
+            temp.push_back(nums[i]);
+            solve(temp,res,dp,nums);
+            dp[i]=0;
+            temp.pop_back();
+        }
+    }
 }
     vector<vector<int>> permute(vector<int>& nums) {
-        int n=nums.size();
-        vector<vector<int>>ans;
+        sort(nums.begin(),nums.end());
+        vector<vector<int>>res;
         vector<int>temp;
-        vector<int>vis(n ,0);
-        solve(temp,nums,vis,ans);
-        return ans;
+        vector<int>dp(nums.size(),0);
+        solve(temp,res,dp,nums);
+        return res;
     }
 };
