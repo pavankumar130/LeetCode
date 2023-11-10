c0a793bba1bff123b58f5f93b7521151cad09386
meravath pavan kumar <122618703+pavankumar130@users.noreply.github.com>
2023-11-10 18:08:02 +0530

    Time: 0 ms (100.00%) | Memory: 6.8 MB (60.71%) - LeetSync

---
+++ 67-add-binary/add-binary.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        if(a.size() == 0){
+            return b;
+        }
+        if(b.size() == 0){
+            return a;
+        }
+        string res;
+        int carry = 0;
+        int i=a.size()-1;
+        int j=b.size()-1;
+        while( i >= 0 || j >= 0  || carry){
+            int sum=carry;
+            if(i >= 0){
+               sum+=a[i--]-'0';
+            }
+            if(j >= 0){
+               sum+=b[j--]-'0';
+            }
+            carry = sum/2;
+            sum = sum%2;
+          res.push_back(sum + '0');
+        }
+        reverse(res.begin(),res.end());
+        return res;
+    }
+};
