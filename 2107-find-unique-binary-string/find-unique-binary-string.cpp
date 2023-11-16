44039d5adda4b47b93147bdba952ae7b7c7f2941
meravath pavan kumar <122618703+pavankumar130@users.noreply.github.com>
2023-11-16 09:13:12 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (52.42%) - LeetSync

--- 2107-find-unique-binary-string/find-unique-binary-string.cpp
+++ 2107-find-unique-binary-string/find-unique-binary-string.cpp
@@ -1,29 +1,32 @@
 class Solution {
 public:
-    string findDifferentBinaryString(vector<string>& nums) {
-        string result = "";
-        string curr = "";
-        bool found = false;
-        int n = nums[0].size();
-        unordered_set<string> S(nums.begin(), nums.end());
-        solve(S, result, curr, n, found);
-        return result;
+void solve(string &curr, string& res, bool &found, int &n, unordered_set<string>&st){
+    if(found){
+        return;
+    }
+    if(curr.size() == n){
+        if(st.find(curr) == st.end()){
+            res=curr;
+            found=true;
+        }
+        return;
     }
 
-private:
-    void solve(unordered_set<string>& S, string& result, string& curr, int& n, bool& found) {
-        if (found) return;
-        if (curr.size() == n) {
-            if (S.find(curr) == S.end())
-                result = curr;
-            return;
-        }
-
-        curr += '0';
-        solve(S, result, curr, n, found);
-        curr.pop_back();
-        curr += '1';
-        solve(S, result, curr, n, found);
-        curr.pop_back();
+    curr+='0';
+    solve(curr,res,found,n,st);
+    curr.pop_back();
+    curr+='1';
+    solve(curr,res,found,n,st);
+    curr.pop_back();
+}
+    string findDifferentBinaryString(vector<string>& nums) {
+        
+        string res="";
+        string curr="";
+        bool found=false;
+        unordered_set<string>st(nums.begin(),nums.end());
+        int n=nums[0].size();
+        solve(curr,res,found,n,st);
+        return res;
     }
 };
