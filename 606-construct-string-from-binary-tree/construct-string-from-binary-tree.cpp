2f1997d20a8c3b0266db013343c8bd3655880017
meravath pavan kumar <122618703+pavankumar130@users.noreply.github.com>
2023-12-08 08:14:52 +0530

    Time: 12 ms (73.57%) | Memory: 56.9 MB (19.47%) - LeetSync

---
+++ 606-construct-string-from-binary-tree/construct-string-from-binary-tree.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    string tree2str(TreeNode* root) {
+        if(root == NULL){
+            return "";
+        }
+
+        string res = to_string(root->val);
+
+        string left = tree2str(root->left);
+        string right = tree2str(root->right);
+
+        if(left.size() == 0 && right.size() == 0){
+            return res;
+        }
+        if(left.size() == 0){
+            return res + "()" + "(" + right + ")";
+        }
+        if(right.size() == 0){
+            return res + "(" + left + ")";
+        }
+        return res +  "(" + left + ")"+ "(" + right + ")";
+    }
+};
