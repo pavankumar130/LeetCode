4ae1f0584d88c8519a57be98c691aba8c6005559
meravath pavan kumar <122618703+pavankumar130@users.noreply.github.com>
2023-12-09 20:47:54 +0530

    Time: 3 ms (88.42%) | Memory: 21.3 MB (11.02%) - LeetSync

---
+++ 3221-find-the-peaks/find-the-peaks.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> findPeaks(vector<int>& mountain) {
+        vector<int>res;
+        
+        int n=mountain.size();
+
+        for(int i=1; i<n-1; i++){
+
+            if(mountain[i-1] < mountain[i] && mountain[i+1] < mountain[i]){
+                res.push_back(i);
+            }
+        }
+        return res;
+    }
+};
