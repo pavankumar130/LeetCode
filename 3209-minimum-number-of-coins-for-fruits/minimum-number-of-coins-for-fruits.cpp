1ced30a487f61e03472f6da13a9897ad492e0649
meravath pavan kumar <122618703+pavankumar130@users.noreply.github.com>
2023-12-10 11:46:55 +0530

    Time: 10 ms (97.18%) | Memory: 20.3 MB (73.27%) - LeetSync

--- 3209-minimum-number-of-coins-for-fruits/minimum-number-of-coins-for-fruits.cpp
+++ 3209-minimum-number-of-coins-for-fruits/minimum-number-of-coins-for-fruits.cpp
@@ -1,23 +1,19 @@
 class Solution {
 public:
-int solve(int i, int n, vector<int>&prices, vector<int>&dp){
-    if(i>=n){
-        return 0;
-    }
-
-    if(dp[i] != -1){
-        return dp[i];
-    }
-
-    int mini = 1e9;
-    for(int j=i+1; j <= min(n,2*(i+1)); j++){
-       mini = min(mini, solve(j,n,prices,dp));
-    }
-    return dp[i]= mini+prices[i];
-}
     int minimumCoins(vector<int>& prices) {
         int n=prices.size();
-        vector<int>dp(n,-1);
-        return solve(0,n,prices,dp);
+
+        vector<int>dp(n+1, -1);
+        dp[n]=0;
+        dp[n-1]=prices[n-1];
+
+        for(int i=n-2; i>=0; i--){
+            int mini = 1e9;
+            for(int j = i+1; j<= min(n, 2*(i+1));  j++){
+                mini = min(mini,dp[j]);
+            }
+            dp[i]= prices[i]+ mini;
+        }
+        return dp[0];
     }
 };
