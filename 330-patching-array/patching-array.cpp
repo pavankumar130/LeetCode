bee876d9684300802c41c4bc63aa01805ab7e284
meravath pavan kumar <122618703+pavankumar130@users.noreply.github.com>
2023-12-10 12:42:24 +0530

    Time: 6 ms (38.75%) | Memory: 11.8 MB (12.05%) - LeetSync

---
+++ 330-patching-array/patching-array.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int minPatches(vector<int>& nums, int n) {
+        
+        long long prefix =0;
+        sort(nums.begin(),nums.end());
+        int size = nums.size(),res = 0, id = 0;
+
+        for(long long t=1; t<=n; t++){
+            if(id < size && nums[id] <= t){
+                prefix+=nums[id];
+                id++;
+            }
+            else{
+                if(prefix >= t){
+                    t = prefix;
+                    continue;
+                }
+                prefix+=t;
+                res++;
+            }
+        }
+        return res;
+    }
+};
