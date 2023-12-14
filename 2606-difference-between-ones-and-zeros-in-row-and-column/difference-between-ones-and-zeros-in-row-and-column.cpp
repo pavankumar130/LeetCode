5ac949469a85d8e9fbc90d490b6b98f34139f915
meravath pavan kumar <122618703+pavankumar130@users.noreply.github.com>
2023-12-14 08:33:52 +0530

    Time: 187 ms (78.16%) | Memory: 117.6 MB (65.73%) - LeetSync

--- 2606-difference-between-ones-and-zeros-in-row-and-column/difference-between-ones-and-zeros-in-row-and-column.cpp
+++ 2606-difference-between-ones-and-zeros-in-row-and-column/difference-between-ones-and-zeros-in-row-and-column.cpp
@@ -6,20 +6,13 @@ public:
         int m=grid[0].size();
 
         vector<int>onesrow(n,0);
-        vector<int>zerosrow(n,0);
-
         vector<int>onescol(m,0);
-        vector<int>zeroscol(m,0);
 
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
-                if(grid[i][j] == 0){
-                    zerosrow[i]++;
-                    zeroscol[j]++;
-                }
-                else{
-                    onesrow[i]++;
-                    onescol[j]++;
+                if(grid[i][j] == 1){
+                   onesrow[i]++;
+                   onescol[j]++;
                 }
             }
         }
@@ -28,7 +21,7 @@ public:
 
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
-                int num = (onesrow[i]+onescol[j])-(zerosrow[i]+zeroscol[j]);
+                int num = ((2*onesrow[i])+(2*onescol[j])- (n+m));
                 res[i][j]=num;
             }
         }
