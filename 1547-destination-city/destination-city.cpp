06cbe806b5304910a09b2ce45e2a08c9fa1e3093
meravath pavan kumar <122618703+pavankumar130@users.noreply.github.com>
2023-12-15 08:19:20 +0530

    Time: 6 ms (92.58%) | Memory: 10.9 MB (85.89%) - LeetSync

--- 1547-destination-city/destination-city.cpp
+++ 1547-destination-city/destination-city.cpp
@@ -3,19 +3,15 @@ public:
     string destCity(vector<vector<string>>& paths) {
         
         int n=paths.size();
+        
+        unordered_set<string>vis;
+        for(int i=0; i<n; i++){
+            vis.insert(paths[i][0]);
+        }
 
         for(int i=0; i<n; i++){
             string res=paths[i][1];
-            bool flag=true;
-
-            for(int j=0; j<n; j++){
-                if(paths[j][0] == res){
-                    flag = false;
-                    break;
-                }
-            }
-
-            if(flag){
+            if(vis.find(res) == vis.end()){
                 return res;
             }
         }
