fbc900d0e9b09a9ace6dc98e995768c576c51070
meravath pavan kumar <122618703+pavankumar130@users.noreply.github.com>
2023-12-19 10:16:20 +0530

    Time: 42 ms (63.69%) | Memory: 22 MB (59.38%) - LeetSync

---
+++ 661-image-smoother/image-smoother.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<vector<int>> imageSmoother(vector<vector<int>>& img) {
+        int n=img.size();
+        int m=img[0].size();
+
+        vector<vector<int>>res(n,vector<int>(m,0));
+
+        int dr[]={-1,-1,-1,0,1,1,1,0};
+        int dc[]={-1,0,1,1,1,0,-1,-1};
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                int cnt = 1;
+                int avg = img[i][j];
+                for(int ind=0; ind<8; ind++){
+                    int x = i+dr[ind];
+                    int y = j +dc[ind];
+                    if(x>=0 && x<n && y>=0 && y<m){
+                        cnt++;
+                        avg+=img[x][y];
+                    }
+                }
+
+                int num = avg/cnt;
+                res[i][j]=num;
+            }
+        }
+        return res;
+    }
+};
