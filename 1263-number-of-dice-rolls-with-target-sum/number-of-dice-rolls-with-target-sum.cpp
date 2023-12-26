9440388d4f14062e764e23eacdf8cba877fc1f4c
meravath pavan kumar <122618703+pavankumar130@users.noreply.github.com>
2023-12-26 09:54:55 +0530

    Time: 24 ms (63.55%) | Memory: 8.9 MB (45.95%) - LeetSync

--- 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.cpp
+++ 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.cpp
@@ -5,7 +5,7 @@ int solve(int n, int k, int target,vector<vector<int>>&dp){
     if(n == 0 && target == 0){
         return 1;
     }
-    if(n<0 || target <= 0){
+    if(n<0 || target < 0){
         return 0;
     }
     if(dp[n][target] != -1){
