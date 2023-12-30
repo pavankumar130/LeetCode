1f4d9dd42c13c343bf6d7b76a0ce53b13dacbc59
meravath pavan kumar <122618703+pavankumar130@users.noreply.github.com>
2023-12-30 10:15:45 +0530

    Time: 17 ms (35.80%) | Memory: 11.9 MB (81.71%) - LeetSync

--- 2025-redistribute-characters-to-make-all-strings-equal/redistribute-characters-to-make-all-strings-equal.cpp
+++ 2025-redistribute-characters-to-make-all-strings-equal/redistribute-characters-to-make-all-strings-equal.cpp
@@ -3,16 +3,16 @@ public:
     bool makeEqual(vector<string>& words) {
         int n = words.size();
 
-        unordered_map<char,int>mp;
+        vector<int>freq(26,0);
 
         for(int i=0; i<words.size(); i++){
             for(int j=0; j<words[i].size(); j++){
-                mp[words[i][j]]++;
+                freq[words[i][j]-'a']++;
             }
         }
 
-        for(auto it = mp.begin(); it != mp.end(); it++){
-            if((it->second%n) != 0){
+        for(int i=0; i<26; i++){
+            if(freq[i]%n != 0){
                 return false;
             }
         }
