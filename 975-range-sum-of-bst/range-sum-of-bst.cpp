a43a302d0f29419f142889dc1bc6804bc806edd8
meravath pavan kumar <122618703+pavankumar130@users.noreply.github.com>
2024-01-08 16:55:09 +0530

    Time: 113 ms (17.82%) | Memory: 65 MB (51.38%) - LeetSync

--- 975-range-sum-of-bst/range-sum-of-bst.cpp
+++ 975-range-sum-of-bst/range-sum-of-bst.cpp
@@ -10,25 +10,18 @@
  * };
  */
 class Solution {
-    void solve(vector<int>&arr, TreeNode* root){
-        if(root == NULL){
-            return;
-        }
-        solve(arr,root->left);
-        arr.push_back(root->val);
-        solve(arr,root->right);
-    }
 public:
     int rangeSumBST(TreeNode* root, int low, int high) {
-        vector<int>arr;
-        solve(arr,root);
-
-        int res = 0;
-        for(int i=0; i<arr.size(); i++){
-            if(arr[i] >= low && arr[i] <= high){
-                res +=arr[i];
-            }
+        if(root == NULL){
+            return 0;
         }
-        return res;
+        int res = 0;
+        if(root->val >= low && root->val <= high){
+            res += root->val;
+        }
+        int left = rangeSumBST(root->left,low,high);
+        int right = rangeSumBST(root->right,low,high);
+
+        return (res+left+right);
     }
 };
