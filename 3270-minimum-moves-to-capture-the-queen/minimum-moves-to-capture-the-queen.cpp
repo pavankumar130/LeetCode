f9dab4244b9f2c48a2e5352bee788fd38f545f6a
meravath pavan kumar <122618703+pavankumar130@users.noreply.github.com>
2024-01-19 23:35:52 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (8.86%) - LeetSync

---
+++ 3270-minimum-moves-to-capture-the-queen/minimum-moves-to-capture-the-queen.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int minMovesToCaptureTheQueen(int a, int b, int c, int d, int e, int f) {
+        if(a == e || b == f){
+            if( a == e && a == c && (d-b)*(d-f) < 0){
+                return 2;
+            }
+            if( b == f &&  b == d && (c-a)*(c-e) < 0){
+                return 2;
+            }
+            return 1;
+        }
+
+        if(abs(c-e) == abs(d-f)){
+            if(abs(c-a) == abs(d-b) && (b-f)*(b-d) < 0){
+                return 2;
+            }
+            return 1;
+        }
+      return 2;
+    }
+};
