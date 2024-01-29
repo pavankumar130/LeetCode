f0a6b85fd639b2bb1da53028b40aec47524720c2
meravath pavan kumar <122618703+pavankumar130@users.noreply.github.com>
2024-01-29 11:48:46 +0530

    Time: 3 ms (40.09%) | Memory: 8.4 MB (5.23%) - LeetSync

---
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.cpp
@@ -0,0 +1,63 @@
+class MyQueue {
+    stack<int>st1,st2;
+public:
+    MyQueue() {
+        
+    }
+    
+    void push(int x) {
+       st1.push(x); 
+    }
+    
+    int pop() {
+       if(st1.empty()){
+           return -1;
+       } 
+
+       while(st1.size() > 1){
+           st2.push(st1.top());
+           st1.pop();
+       }
+
+       int val = st1.top();
+       st1.pop();
+
+       while(st2.size()){
+           st1.push(st2.top());
+           st2.pop();
+       }
+       return val;
+    }
+    
+    int peek() {
+        if(st1.empty()){
+           return -1;
+       } 
+
+       while(st1.size() > 1){
+           st2.push(st1.top());
+           st1.pop();
+       }
+
+       int val = st1.top();
+
+       while(st2.size()){
+           st1.push(st2.top());
+           st2.pop();
+       }
+       return val;
+    }
+    
+    bool empty() {
+        return st1.empty();
+    }
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue* obj = new MyQueue();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->empty();
+ */
