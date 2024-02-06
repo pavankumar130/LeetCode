1db592559a31a247723703368a2576a8f9975f9d
meravath pavan kumar <122618703+pavankumar130@users.noreply.github.com>
2024-02-06 22:31:11 +0530

    Time: 2 ms (47.55%) | Memory: 8.5 MB (37.36%) - LeetSync

--- 3297-minimum-time-to-revert-word-to-initial-state-i/minimum-time-to-revert-word-to-initial-state-i.cpp
+++ 3297-minimum-time-to-revert-word-to-initial-state-i/minimum-time-to-revert-word-to-initial-state-i.cpp
@@ -1,19 +1,12 @@
 class Solution {
 public:
     int minimumTimeToInitialState(string word, int k) {
-        int res = 1;
-        int n = word.size();
-
+        int n = word.length();
         for(int i = k; i < n; i+=k){
-            string s1 = word.substr(i,n-i);
-            string s2 = word.substr(0,n-i);
-            if(s1 == s2){
-                return res;
-            }
-            else{
-                res++;
+            if(word.substr(0,n-i) == word.substr(i,n-i)){
+                return i/k;
             }
         }
-        return res;
+        return (n+k-1)/k;
     }
 };
