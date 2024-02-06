db94706b400e4904d2b72c3be30d1f3f002d3631
meravath pavan kumar <122618703+pavankumar130@users.noreply.github.com>
2024-02-06 14:43:40 +0530

    Time: 31 ms (62.17%) | Memory: 32.2 MB (43.81%) - LeetSync

--- 3278-find-the-number-of-ways-to-place-people-i/find-the-number-of-ways-to-place-people-i.cpp
+++ 3278-find-the-number-of-ways-to-place-people-i/find-the-number-of-ways-to-place-people-i.cpp
@@ -1,45 +1,30 @@
 class Solution {
-    bool check(int j1, int j2, vector<vector<int>>& points){
-        int n = points.size();
-
-        int x1 = points[j1][0];
-        int y1 = points[j1][1];
-        int x2 = points[j2][0];
-        int y2 = points[j2][1];
-
-        for(int i = 0; i < n; i++){
-            if(i == j1 || i == j2){
-                continue;
-            }
-            int x = points[i][0];
-            int y = points[i][1];
-            if( x >= x1 && x <= x2 && y <= y1 && y >= y2){
-                return false;
-            }
-        }
-        return true;
-    }
 public:
     int numberOfPairs(vector<vector<int>>& points) {
         int n = points.size();
         int res = 0;
-
         for(int i = 0; i < n; i++){
+            int cx = points[i][0], cy = points[i][1];
             for(int j = 0; j < n; j++){
-                if(i == j){
-                    continue;
-                }
-                int x1 = points[i][0];
-                int y1 = points[i][1];
-                int x2 = points[j][0];
-                int y2 = points[j][1];
-
-                if(x2 < x1 || y2 > y1){
+                int tx = points[j][0], ty = points[j][1];
+                if(i == j || tx < cx || ty > cy){
                     continue;
                 }
 
-                if( (((y2-y1) >= 0 && (x2 - x1) <= 0 ) || (y2 - y1) <= 0 && (x2-x1) >= 0 ) 
-                     && check(i,j,points) ){
+                bool flag = true;
+                for(int k = 0; k < n; k++){
+                    if(i == k || j == k){
+                        continue;
+                    }
+
+                    if(points[k][0] <= tx && points[k][0] >= cx &&
+                       points[k][1] <= cy && points[k][1] >=ty){
+                           flag = false;
+                           break;
+                       }
+                }
+
+                if(flag){
                     res++;
                 }
             }
