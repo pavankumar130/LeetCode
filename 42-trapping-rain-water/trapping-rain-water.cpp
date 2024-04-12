760301940241bc3f780709219e832595b37ab99c
meravath pavan kumar <122618703+pavankumar130@users.noreply.github.com>
2024-04-12 09:15:29 +0530

    Time: 18 ms (11.08%) | Memory: 23.2 MB (13.23%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,24 +1,35 @@
 class Solution {
 public:
+vector<int> findright(vector<int>& height){
+    int n=height.size();
+    vector<int>arr(n);
+    arr[n-1]=height[n-1];
+    for(int i=n-2;i>=0;i--){
+       arr[i]=max(arr[i+1],height[i]);
+    }
+    return arr;
+}
+vector<int> findleft(vector<int>& height){
+    int n=height.size();
+    vector<int>arr(n);
+    arr[0]=height[0];
+    for(int i=1;i<n;i++){
+       arr[i]=max(arr[i-1],height[i]);
+    }
+    return arr;
+}
     int trap(vector<int>& height) {
         int n=height.size();
-        vector<int>left(n);
-        vector<int>right(n);
-        int water=0;
-        left[0]=height[0];
-        for(int i=1;i<n;i++){
-            left[i]=max(left[i-1],height[i]);
-        }
-        right[n-1]=height[n-1];
-        for(int i=n-2;i>=0;i--){
-            right[i]=max(right[i+1],height[i]);
-        }
+        vector<int>left= findleft(height);
+        vector<int>right= findright(height);
+
+        int res=0;
         for(int i=1;i<n-1;i++){
-            int val=min(right[i+1],left[i-1]);
+            int val=min(left[i],right[i]);
             if(val > height[i]){
-               water+=val-height[i];
+                res+=val-height[i];
             }
         }
-        return water;
+        return res;
     }
 };
