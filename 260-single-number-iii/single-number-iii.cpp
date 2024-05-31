eaff0181cf049666ec6266fa4a999a4c444ce1cf
meravath pavan kumar <122618703+pavankumar130@users.noreply.github.com>
2024-05-31 09:48:27 +0530

    Time: 10 ms (38.25%) | Memory: 13.4 MB (30.38%) - LeetSync

--- 260-single-number-iii/single-number-iii.cpp
+++ 260-single-number-iii/single-number-iii.cpp
@@ -2,15 +2,18 @@ class Solution {
 public:
     vector<int> singleNumber(vector<int>& nums) {
         map<int,int>mp;
-        for(auto it:nums){
+
+        for(auto it: nums){
             mp[it]++;
         }
+
         vector<int>ans;
-        for(auto it:mp){
+        for(auto it: mp){
             if(it.second == 1){
                 ans.push_back(it.first);
             }
         }
+
         return ans;
     }
 };
