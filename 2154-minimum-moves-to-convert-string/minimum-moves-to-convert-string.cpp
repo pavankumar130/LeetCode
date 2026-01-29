a010d43a79c2b6338dd4c5a86bc05f2eb5fed4a0
meravath pavan kumar <122618703+pavankumar130@users.noreply.github.com>
2026-01-29 22:28:32 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (33.46%) - LeetSync

---
+++ 2154-minimum-moves-to-convert-string/minimum-moves-to-convert-string.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int minimumMoves(string s) {
+        int n = s.size();
+        int cnt = 0;
+        int i = 0;
+
+        while(i < n){
+            if(s[i] == 'X'){
+                cnt++;
+                i += 3;
+            }
+            else{
+                i++;
+            }
+        }
+
+        return cnt;
+    }
+};
