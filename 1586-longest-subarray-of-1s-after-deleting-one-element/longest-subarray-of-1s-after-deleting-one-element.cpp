6487ffe67ed1c8204ec07cf5dcaa7600b1facef9
meravath pavan kumar <122618703+pavankumar130@users.noreply.github.com>
2026-02-08 16:24:41 +0530

    Time: 0 ms (100.00%) | Memory: 60.2 MB (48.60%) - LeetSync

--- 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.cpp
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.cpp
@@ -2,36 +2,23 @@ class Solution {
 public:
     int longestSubarray(vector<int>& nums) {
         int n = nums.size();
+        int left = 0, zeros = 0, res = 0;
 
-        vector<int>left (n + 1, 0);
-        vector<int>right(n + 2, 0);
-
-        for(int i = 1; i <= n; i++){
-            if(nums[i - 1] == 1){
-                left[i] = left[i - 1] + 1;
+        for(int right = 0; right < n; right++){
+            if(nums[right] == 0){
+                zeros++;
             }
-        }
 
-        for(int i = n; i >= 1; i--){
-            if(nums[i - 1] == 1){
-                right[i] = right[i + 1] + 1;
+            while(zeros > 1){
+                if(nums[left] == 0){
+                    zeros--;
+                }
+                left++;
             }
+
+            res = max(res, right - left);
         }
 
-        if(left[n] == n){
-            return n - 1;
-        }
-
-        int maxi = 0;
-
-        for(int i = 0; i < n; i++){
-            if(nums[i] == 0){
-                int sub_arr = left[i] + right[i + 2];
-                //cout<<left[i + 1]<<" "<<right[i + 1]<<" "<<maxi<<endl;
-                maxi = max(maxi, sub_arr);
-            }
-        }
-
-        return maxi;
+        return res;
     }
 };
