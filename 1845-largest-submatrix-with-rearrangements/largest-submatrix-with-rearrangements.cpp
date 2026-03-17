a431de9d629b1eb02cf82ee7d06bb8d15fd0920a
meravath pavan kumar <122618703+pavankumar130@users.noreply.github.com>
2026-03-17 22:24:16 +0530

    Time: 53 ms (26.06%) | Memory: 101.6 MB (14.89%) - LeetSync

--- 1845-largest-submatrix-with-rearrangements/largest-submatrix-with-rearrangements.cpp
+++ 1845-largest-submatrix-with-rearrangements/largest-submatrix-with-rearrangements.cpp
@@ -1,34 +1,25 @@
 class Solution {
 public:
     int largestSubmatrix(vector<vector<int>>& matrix) {
-        
-        vector<vector<int>>temp(matrix.size());
+        vector<vector<int>>mat = matrix;
+        int m = matrix.size();
+        int n = matrix[0].size();
+        int res = 0;
 
-        for(int j=0; j<matrix[0].size(); j++){
-            int dp=0;
-
-            for(int i=0; i<matrix.size(); i++){
-                if(matrix[i][j] == 0){
-                    dp=0;
+        for(int row = 0; row < m; row++){
+            for(int col = 0; col < n; col++){
+                if(matrix[row][col] && row > 0){
+                    matrix[row][col] += matrix[row - 1][col];
                 }
-                else{
-                    dp++;
-                }
-                temp[i].push_back(dp);
             }
 
-        }
-
-        int res=0;
-
-        for(int i=0; i<temp.size(); i++){
-            sort(temp[i].begin(),temp[i].end(),greater<int>{});
-            int row_ans=0;
-            for(int j=0; j<temp[i].size(); j++){
-                row_ans = max(row_ans, temp[i][j]*(j+1));
+            vector<int>currrow = matrix[row];
+            sort(currrow.begin(), currrow.end(), greater());
+            for(int i = 0; i < n; i++){
+                res = max(res, currrow[i] * (i + 1));
             }
-            res=max(res,row_ans);
         }
+
         return res;
     }
 };
